8ee85431f55941dd78e7dd4f7de0a2e0d8379c80
AntonioSilva <antonio.ifusp@gmail.com>
2020-04-09 13:42:11 -0400

    changed RunAnalysis.sh paths

--- Nolan/RunAnalysis.sh
+++ Nolan/RunAnalysis.sh
@@ -1,14 +1,14 @@
  #!/bin/bash
-ANALYSIS_DIR=/home/antoniosilva/RivetWorkdir/Nolan
-PYTHIA_EX_DIR=/home/antoniosilva/RivetWorkdir/Pythia8/pythia8243/examples
-cd $ANALYSIS_DIR/Centrality
-rm *.so
-export RIVET_ANALYSIS_PATH=$PWD
-rivet-buildplugin RivetRHIC_2019_CentralityCalibration.so RHIC_2019_CentralityCalibration.cc
-rivet --pwd -a RHIC_2019_CentralityCalibration:exp=STAR -o calibration_AuAu_200GeV_STAR.yoda --ignore-beams $PYTHIA_EX_DIR/hepmc_AuAu_MB_1.hepmc $PYTHIA_EX_DIR/hepmc_AuAu_MB_2.hepmc $PYTHIA_EX_DIR/hepmc_AuAu_MB_3.hepmc $PYTHIA_EX_DIR
-cd ..
+ANALYSIS_DIR=/nics/c/home/aolivei6/Nolan
+PYTHIA_EX_DIR=/nics/a/proj/UTK0019/Pythia8/pythia8243/examples
+#cd $ANALYSIS_DIR/Centrality
+#rm *.so
+#export RIVET_ANALYSIS_PATH=$PWD
+#rivet-buildplugin RivetRHIC_2019_CentralityCalibration.so RHIC_2019_CentralityCalibration.cc
+#rivet --pwd -a RHIC_2019_CentralityCalibration:exp=STAR -o calibration_AuAu_200GeV_STAR.yoda --ignore-beams $PYTHIA_EX_DIR/hepmc_AuAu_MB_1.hepmc $PYTHIA_EX_DIR/hepmc_AuAu_MB_2.hepmc $PYTHIA_EX_DIR/hepmc_AuAu_MB_3.hepmc $PYTHIA_EX_DIR
+#cd ..
 rm *.so
 export RIVET_ANALYSIS_PATH=$PWD
 rivet-buildplugin RivetNolan_2020_I0000000.so Nolan_2020_I0000000.cc
-rivet --pwd -p $ANALYSIS_DIR/Centrality/calibration_AuAu_200GeV_STAR.yoda -a Nolan_2020_I0000000:cent=GEN -o Angantyr.yoda $PYTHIA_EX_DIR/hepmc_AuAu_MB_1.hepmc
-#rivet-mkhtml --pwd Rivet.yoda 
+rivet --pwd -p $ANALYSIS_DIR/Centrality/calibration_AuAu_200GeV_STAR.yoda -a Nolan_2020_I0000000:cent=GEN -o Angantyr.yoda $PYTHIA_EX_DIR/hepmc_AuAu_200GeV_500Events.hepmc
+#rivet-mkhtml --pwd Rivet.yoda
