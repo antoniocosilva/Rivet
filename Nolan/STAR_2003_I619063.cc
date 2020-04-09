8661bff7ba81db265f6aee0fde5ddf2b529b59f1
AntonioSilva <antonio.ifusp@gmail.com>
2020-04-09 15:23:51 -0400

    Added histos from HepData

--- Nolan/STAR_2003_I619063.cc
+++ Nolan/STAR_2003_I619063.cc
@@ -37,11 +37,28 @@ namespace Rivet {
 
       // Book histograms
       // specify custom binning
-      book(chSpectrum["chSpectrum0_5"], "chSpectrum0_5", 50, 0., 50);
-      book(chSpectrum["chSpectrum5_10"], "chSpectrum5_10", 50, 0., 50);
+      book(chSpectrum["chSpectrum0_5"], 1, 1, 1);
+      book(chSpectrum["chSpectrum5_10"], 1, 1, 2);
+      book(chSpectrum["chSpectrum10_20"], 1, 1, 3);
+      book(chSpectrum["chSpectrum20_30"], 1, 1, 4);
+      book(chSpectrum["chSpectrum30_40"], 1, 1, 5);
+      book(chSpectrum["chSpectrum40_60"], 1, 1, 6);
       book(sow["sow0_5"], "sow0_5");
       book(sow["sow5_10"], "sow5_10");
+      book(sow["sow0_5"], "sow10_20");
+      book(sow["sow5_10"], "sow20_30");
+      book(sow["sow0_5"], "sow30_4");
+      book(sow["sow5_10"], "sow40_60");
+      book(sow["sow5_10"], "sow60_80");
       book(Rcp["Rcp0_5_over_5_10"], "Rcp0_5_over_5_10");
+      
+      string refnameCentRatio05_4060 = mkAxisCode(4,1,1);
+      book(Rcp["Rcp0_5_over_40_60"], refnameCentRatio05_4060);
+      
+      string refnameCentRatio05_6080 = mkAxisCode(4,1,2);
+      const Scatter2D& refdataCentRatio05_6080 = refData(refnameCentRatio05_6080);
+      book(chSpectrum["chSpectrum60_80"], refnameCentRatio05_6080 + "_6080", refdataCentRatio05_6080);
+      book(Rcp["Rcp0_5_over_60_80"], refnameCentRatio05_6080);
       
       // take binning from reference data using HEPData ID (digits in "d01-x01-y01" etc.)
       //book(_h["AAAA"], 1, 1, 1);
@@ -76,6 +93,31 @@ namespace Rivet {
               chSpectrum["chSpectrum5_10"]->fill(p.pT()/GeV);
               sow["sow5_10"]->fill();
           }
+          else if(c >= 10 && c < 20)
+          {
+              chSpectrum["chSpectrum10_20"]->fill(p.pT()/GeV);
+              sow["sow10_20"]->fill();
+          }
+          else if(c >= 20 && c < 30)
+          {
+              chSpectrum["chSpectrum20_30"]->fill(p.pT()/GeV);
+              sow["sow20_30"]->fill();
+          }
+          else if(c >= 30 && c < 40)
+          {
+              chSpectrum["chSpectrum30_40"]->fill(p.pT()/GeV);
+              sow["sow30_40"]->fill();
+          }
+          else if(c >= 40 && c < 60)
+          {
+              chSpectrum["chSpectrum40_60"]->fill(p.pT()/GeV);
+              sow["sow40_60"]->fill();
+          }
+          else if(c >= 60 && c < 80)
+          {
+              chSpectrum["chSpectrum60_80"]->fill(p.pT()/GeV);
+              sow["sow60_80"]->fill();
+          }
       }
         
 
@@ -87,12 +129,18 @@ namespace Rivet {
 
       chSpectrum["chSpectrum0_5"]->scaleW(1./sow["sow0_5"]->sumW());
       chSpectrum["chSpectrum5_10"]->scaleW(1./sow["sow5_10"]->sumW());
+      chSpectrum["chSpectrum10_20"]->scaleW(1./sow["sow10_20"]->sumW());
+      chSpectrum["chSpectrum20_30"]->scaleW(1./sow["sow20_30"]->sumW());
+      chSpectrum["chSpectrum30_40"]->scaleW(1./sow["sow30_40"]->sumW());
+      chSpectrum["chSpectrum40_60"]->scaleW(1./sow["sow40_60"]->sumW());
+      chSpectrum["chSpectrum60_80"]->scaleW(1./sow["sow60_80"]->sumW());
         
       //normalize(_h["YYYY"]); // normalize to unity
       //scale(_h["ZZZZ"], crossSection()/picobarn/sumOfWeights()); // norm to cross section
       
       //
-      divide(chSpectrum["chSpectrum0_5"], chSpectrum["chSpectrum5_10"], Rcp["Rcp0_5_over_5_10"]);
+      divide(chSpectrum["chSpectrum0_5"], chSpectrum["chSpectrum40_60"], Rcp["Rcp0_5_over_40_60"]);
+      divide(chSpectrum["chSpectrum0_5"], chSpectrum["chSpectrum60_80"], Rcp["Rcp0_5_over_60_80"]);
 
     }
 
