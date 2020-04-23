c856091b93160bf8f4de9a3583c25dd42735fa0d
Christine Nattrass <cnattras@acf-login7.acf.nics.utk.edu>
2020-04-23 13:48:21 -0400

    Adding Nolans changes

--- Nolan/STAR_2003_I619063.cc
+++ Nolan/STAR_2003_I619063.cc
@@ -32,7 +32,7 @@ namespace Rivet {
       // all final-state particles within 
       // the given eta acceptance
       //NOLAN: You should change this to the pseudorapidity in the paper
-      const FinalState fs(Cuts::abseta < 4.9 && Cuts::abscharge > 0);
+      const FinalState fs(Cuts::abseta < 0.5 && Cuts::abscharge > 0);
       declare(fs,"fs");
       
 
@@ -49,7 +49,7 @@ namespace Rivet {
       book(sow["sow5_10"], "sow5_10");
       book(sow["sow10_20"], "sow10_20");
       book(sow["sow20_30"], "sow20_30");
-      book(sow["sow30_40"], "sow30_4");
+      book(sow["sow30_40"], "sow30_40");
       book(sow["sow40_60"], "sow40_60");
       book(sow["sow60_80"], "sow60_80");
       
@@ -85,37 +85,37 @@ namespace Rivet {
           if(c < 5.)
           {
 	    //can do p.pT()/GeV,1.0/(p.pT()/GeV).  Add to all centralities.
-              chSpectrum["chSpectrum0_5"]->fill(p.pT()/GeV);
+              chSpectrum["chSpectrum0_5"]->fill(p.pT()/GeV,1.0/(p.pT()/GeV));
               sow["sow0_5"]->fill();
           }
           else if(c >= 5 && c < 10)
           {
-              chSpectrum["chSpectrum5_10"]->fill(p.pT()/GeV);
+              chSpectrum["chSpectrum5_10"]->fill(p.pT()/GeV,1.0/(p.pT()/GeV));
               sow["sow5_10"]->fill();
           }
           else if(c >= 10 && c < 20)
           {
-              chSpectrum["chSpectrum10_20"]->fill(p.pT()/GeV);
+              chSpectrum["chSpectrum10_20"]->fill(p.pT()/GeV,1.0/(p.pT()/GeV));
               sow["sow10_20"]->fill();
           }
           else if(c >= 20 && c < 30)
           {
-              chSpectrum["chSpectrum20_30"]->fill(p.pT()/GeV);
+              chSpectrum["chSpectrum20_30"]->fill(p.pT()/GeV,1.0/(p.pT()/GeV));
               sow["sow20_30"]->fill();
           }
           else if(c >= 30 && c < 40)
           {
-              chSpectrum["chSpectrum30_40"]->fill(p.pT()/GeV);
+              chSpectrum["chSpectrum30_40"]->fill(p.pT()/GeV,1.0/(p.pT()/GeV));
               sow["sow30_40"]->fill();
           }
           else if(c >= 40 && c < 60)
           {
-              chSpectrum["chSpectrum40_60"]->fill(p.pT()/GeV);
+              chSpectrum["chSpectrum40_60"]->fill(p.pT()/GeV,1.0/(p.pT()/GeV));
               sow["sow40_60"]->fill();
           }
           else if(c >= 60 && c < 80)
           {
-              chSpectrum["chSpectrum60_80"]->fill(p.pT()/GeV);
+              chSpectrum["chSpectrum60_80"]->fill(p.pT()/GeV,1.0/(p.pT()/GeV));
               sow["sow60_80"]->fill();
           }
       }
@@ -128,22 +128,26 @@ namespace Rivet {
     void finalize() {
       //These lines normalize per event and pT bin width.
       //You need to also scale by 1.0/(2.0*3.14159*[eta acceptance])
-      chSpectrum["chSpectrum0_5"]->scaleW(1./sow["sow0_5"]->sumW());
-      chSpectrum["chSpectrum5_10"]->scaleW(1./sow["sow5_10"]->sumW());
-      chSpectrum["chSpectrum10_20"]->scaleW(1./sow["sow10_20"]->sumW());
-      chSpectrum["chSpectrum20_30"]->scaleW(1./sow["sow20_30"]->sumW());
-      chSpectrum["chSpectrum30_40"]->scaleW(1./sow["sow30_40"]->sumW());
-      chSpectrum["chSpectrum40_60"]->scaleW(1./sow["sow40_60"]->sumW());
-      chSpectrum["chSpectrum60_80"]->scaleW(1./sow["sow60_80"]->sumW());
+      chSpectrum["chSpectrum0_5"]->scaleW(1./(sow["sow0_5"]->sumW()*2.0*3.14159));
+      chSpectrum["chSpectrum5_10"]->scaleW(1./sow["sow5_10"]->sumW()*2.0*3.14159);
+      chSpectrum["chSpectrum10_20"]->scaleW(1./sow["sow10_20"]->sumW()*2.0*3.14159);
+      chSpectrum["chSpectrum20_30"]->scaleW(1./sow["sow20_30"]->sumW()*2.0*3.14159);
+      chSpectrum["chSpectrum30_40"]->scaleW(1./sow["sow30_40"]->sumW()*2.0*3.14159);
+      chSpectrum["chSpectrum40_60"]->scaleW(1./sow["sow40_60"]->sumW()*2.0*3.14159);
+      chSpectrum["chSpectrum60_80"]->scaleW(1./sow["sow60_80"]->sumW()*2.0*3.14159);
         
       //normalize(_h["YYYY"]); // normalize to unity
       //scale(_h["ZZZZ"], crossSection()/picobarn/sumOfWeights()); // norm to cross section
       
       //Look for TAA (nuclear overlap integral) in the paper.  You want to scale each spectrum by this, so
       //First: scale by [TAA 40-60]/[TAA 0-5]
+      
+ 
       divide(chSpectrum["chSpectrum0_5"], chSpectrum["chSpectrum40_60"], Rcp["Rcp0_5_over_40_60"]);
       //Rcp["Rcp0_5_over_40_60"]->ScaleW(1.0/[your scale factor]);
+      //Rcp["Rcpo_5_over_40_60"] -> scaleW(1.0/((90.0/42.0)/(965./42.)));
       //First: scale by [TAA 60-80]/[TAA 0-5]
+      
       divide(chSpectrum["chSpectrum0_5"], chSpectrum["chSpectrum60_80"], Rcp["Rcp0_5_over_60_80"]);
 
     }
